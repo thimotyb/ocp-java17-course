eb56676e7cc929d5a0d5b5a53f8c3995bfe2e774
Thimoty Barbieri <thimoty@thimoty.it>
2025-10-24 11:10:57 +0200

    ch01: add Chick example and refine NumberPicker demo\n\n- Add Chick illustrating instance initializer + constructor order and field defaulting\n- Update NumberPicker to print a random number and a marker line\n- Include an instance initializer in NumberPicker to note it does not run without instantiation\n\nPart of Chapter 1 language basics examples.

---
+++ chapters/ch01-language-basics/src/main/java/ch01/examples/Chick.java
@@ -0,0 +1,14 @@
+package ch01.examples;
+
+public class Chick {
+     private String name = "Fluffy";
+     { System.out.println("setting field"); }
+     public Chick() {
+        name = "Tiny";
+       System.out.println("setting constructor");
+    }
+    public static void main(String[] args) {
+        Chick chick = new Chick();
+       System.out.println(chick.name); } 
+}
+
--- chapters/ch01-language-basics/src/main/java/ch01/examples/NumberPicker.java
+++ chapters/ch01-language-basics/src/main/java/ch01/examples/NumberPicker.java
@@ -2,10 +2,13 @@ package ch01.examples;
 
 import java.util.Random;
 
+
 public class NumberPicker {
 
     public static void main(String[] args) {
         Random r = new Random();
         System.out.println(r.nextInt(10));
+        System.out.println("Hey");
     }
+    { System.out.println("Snowy"); } // Not executed initializer because the class is never instantiated
 }
